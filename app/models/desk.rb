e32416070959ab00664c00644fea3410c67d7f5b
Eric Dahl <eric@skeyelab.com>
2017-04-13 11:34:05 -0500

    desk model

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -1,3 +1,13 @@
 class Desk < ApplicationRecord
   attr_encrypted :token, key: ENV["ENC_KEY"]
+
+  after_initialize :defaults, unless: :persisted?
+
+  def defaults
+    self.last_timestamp||=0
+    self.last_timestamp_event||=0
+    self.wait_till||=0
+    self.wait_till_event||=0
+    self.active||=false
+  end
 end
