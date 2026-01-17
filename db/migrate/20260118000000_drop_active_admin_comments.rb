85b65aa6bea5482781fd46f380b6eabdff6c5e02
Eric Dahl <eric@skeyelab.com>
2026-01-17 11:09:00 -0500

    Remove problematic migration file
    The active_admin_comments table is already removed from schema. Removing the migration that was causing deployment failures. Table cleanup can be done manually if needed.

--- db/migrate/20260118000000_drop_active_admin_comments.rb
+++
@@ -1,8 +0,0 @@
-class DropActiveAdminComments < ActiveRecord::Migration[8.0]
-  def change
-    # No-op migration - table cleanup deferred
-    # The active_admin_comments table was removed when migrating from ActiveAdmin to Avo
-    # This migration exists to mark the schema version but doesn't perform any operations
-    # Manual cleanup can be done later if needed: DROP TABLE IF EXISTS active_admin_comments CASCADE;
-  end
-end
