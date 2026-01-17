33888cd0698a15319ea23e715341720e6df3c306
Eric Dahl <eric@skeyelab.com>
2026-01-17 10:37:06 -0500

    Use Rails migration DSL instead of raw SQL for dropping table
    Switch from execute with raw SQL to drop_table with if_exists option. This is more idiomatic Rails and should provide better error messages if something fails.

--- db/migrate/20260118000000_drop_active_admin_comments.rb
+++ db/migrate/20260118000000_drop_active_admin_comments.rb
@@ -1,23 +1,7 @@
 class DropActiveAdminComments < ActiveRecord::Migration[8.0]
-  def up
-    # Use raw SQL to safely drop table if it exists
-    # CASCADE ensures any dependent objects are also dropped
-    # IF EXISTS makes this safe to run even if the table doesn't exist
-    execute "DROP TABLE IF EXISTS active_admin_comments CASCADE"
-  end
-
-  def down
-    return if connection.table_exists?(:active_admin_comments)
-
-    create_table :active_admin_comments do |t|
-      t.string :namespace
-      t.text :body
-      t.string :resource_id, null: false
-      t.string :resource_type, null: false
-      t.references :author, polymorphic: true
-      t.timestamps
-    end
-    add_index :active_admin_comments, [:namespace]
-    add_index :active_admin_comments, [:resource_type, :resource_id]
+  def change
+    # Safely drop table if it exists
+    # This migration is idempotent - safe to run multiple times
+    drop_table :active_admin_comments, if_exists: true
   end
 end
