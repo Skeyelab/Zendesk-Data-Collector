69760f909f8a38aa0f6426a1b540ea79fc466e60
Eric Dahl <eric@skeyelab.com>
2026-01-20 20:09:52 -0500

    Apply standardrb formatting to base_controller_test.rb

--- test/controllers/mission_control/base_controller_test.rb
+++ test/controllers/mission_control/base_controller_test.rb
@@ -1,24 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class MissionControl::BaseControllerTest < ActionDispatch::IntegrationTest
-  test "should require authentication to access Mission Control" do
-    get "/jobs"
+  test 'should require authentication to access Mission Control' do
+    get '/jobs'
     # Devise redirects to sign in path - check redirect status and path
     assert_response :redirect
     assert_match(/sign_in/, response.location)
   end
 
-  test "should allow access when authenticated" do
+  test 'should allow access when authenticated' do
     admin_user = AdminUser.create!(
-      email: "admin@example.com",
-      password: "password123456",
-      password_confirmation: "password123456"
+      email: 'admin@example.com',
+      password: 'password123456',
+      password_confirmation: 'password123456'
     )
 
     # Sign in using Devise test helper with the correct scope
     sign_in admin_user, scope: :admin_user
 
-    get "/jobs"
+    get '/jobs'
     assert_response :success
   end
 end
