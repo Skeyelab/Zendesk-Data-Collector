13ba6f442f477f1abbb5aff59f2115d110e17550
Eric Dahl <eric@skeyelab.com>
2026-02-05 10:50:24 -0500

    Update active_record_encryption.rb

--- config/initializers/active_record_encryption.rb
+++ config/initializers/active_record_encryption.rb
@@ -15,18 +15,18 @@ Rails.application.config.active_record.encryption.primary_key =
   encryption_env("ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY") ||
   Rails.application.credentials.dig(:active_record_encryption, :primary_key) ||
   # Generate a development/test key if neither is set
-  (Rails.env.development? || Rails.env.test? ? SecureRandom.hex(32) : nil)
+  ((Rails.env.development? || Rails.env.test?) ? SecureRandom.hex(32) : nil)
 
 # Deterministic key: 32 bytes for deterministic encryption
 Rails.application.config.active_record.encryption.deterministic_key =
   encryption_env("ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY") ||
   Rails.application.credentials.dig(:active_record_encryption, :deterministic_key) ||
   # Generate a development/test key if neither is set
-  (Rails.env.development? || Rails.env.test? ? SecureRandom.hex(32) : nil)
+  ((Rails.env.development? || Rails.env.test?) ? SecureRandom.hex(32) : nil)
 
 # Key derivation salt: used for key derivation
 Rails.application.config.active_record.encryption.key_derivation_salt =
   encryption_env("ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT") ||
   Rails.application.credentials.dig(:active_record_encryption, :key_derivation_salt) ||
   # Generate a development/test salt if neither is set
-  (Rails.env.development? || Rails.env.test? ? SecureRandom.hex(32) : nil)
+  ((Rails.env.development? || Rails.env.test?) ? SecureRandom.hex(32) : nil)
